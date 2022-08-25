1adc454e3eb4c4350a876af8f84e6a49fe974eb2
Jbernardiss <joaobernardiss@gmail.com>
2022-08-25 01:00:56 -0300

    Third Commit: Implementation of sign button and percentage

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let calc = {
     clearButton: document.getElementById("clearButton"),
     resetButton: document.querySelector(".resetButton"),
     equalButton: document.querySelector(".equalButton"),
+    percentageButton: document.querySelector(".percentageButton"),
+    signButton: document.querySelector(".signButton"),
     operation: "",
     deleteDisplayText: false,
     numBuffer: [],
@@ -25,6 +27,24 @@ let calc = {
 
     clearDisplay: function() {
         this.display.textContent = ""
+    },
+
+    calcPercentage: function() {
+        this.display.textContent = parseFloat((this.display.textContent)/100)  
+    },
+
+    flipSign: function() {
+
+        if(this.display.textContent[0] == "+") {
+            this.display.textContent = this.display.textContent.replace("+", "-")
+            return "-"
+        } else if(this.display.textContent[0] == "-") {
+            this.display.textContent = this.display.textContent.replace("-", "+")
+            return "+"
+        } else if((this.display.textContent[0] != "-") && (this.display.textContent[0] != "+")) {
+            this.display.textContent = `-${this.display.textContent}`
+            return "-"
+        }
     },
 
     resetCalc: function() {
@@ -44,7 +64,7 @@ let calc = {
 
         if(this.operation == "+") {
             this.result = this.numBuffer[0] + this.numBuffer[1]
-        } else if(this.operation == "−") {
+        } else if(this.operation == "-") {
             this.result = this.numBuffer[0] - this.numBuffer[1]
         } else if(this.operation == "×") {
             this.result = this.numBuffer[0] * this.numBuffer[1]
@@ -62,6 +82,8 @@ let calc = {
 
 calc.resetButton.addEventListener("click", () => calc.resetCalc())
 calc.equalButton.addEventListener("click", () => calc.exeOperation())
+calc.percentageButton.addEventListener("click", () => calc.calcPercentage())
+calc.signButton.addEventListener("click", () => calc.flipSign())
 
 for(let button of calc.numButtons) {
     button.addEventListener("click", () => calc.updateDisplay(button))
