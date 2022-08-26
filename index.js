23c78085a1fbe8031207f519c8b3348b20297ee3
Jbernardiss <joaobernardiss@gmail.com>
2022-08-25 23:11:09 -0300

    Fourth Commit: Added comments and corrected issue which prevented that new operations could be done with results of previous operation

--- index.js
+++ index.js
@@ -2,66 +2,108 @@
 
 let calc = {
 
-    display: document.getElementById("display"),
-    buttons: document.getElementsByClassName("buttons"),
-    numButtons: document.getElementsByClassName("numButtons"),
-    opButtons: document.getElementsByClassName("opButtons"),
+    /** Object representing the calculator */
+
+    display: document.getElementById("display"), 
+    buttons: document.getElementsByClassName("buttons"), 
+    numButtons: document.getElementsByClassName("numButtons"), 
+    opButtons: document.getElementsByClassName("opButtons"), 
     clearButton: document.getElementById("clearButton"),
     resetButton: document.querySelector(".resetButton"),
     equalButton: document.querySelector(".equalButton"),
     percentageButton: document.querySelector(".percentageButton"),
     signButton: document.querySelector(".signButton"),
-    operation: "",
-    deleteDisplayText: false,
+    operation: "", // Variable which stores the operation to be executed
+    deleteDisplayText: false, // Variable which controls whether display text should be deleted when inserting a new number or not
+
+     // Buffer which stores the numbere of the operation. It should always contain only 2 number. The first number is stored from the display 
+     // when any of the operation buttons is pressed. The second number is stored from the display when equal button is pressed.
+     // There's no need for more spaces because if you wanna do calculations with the results of the previous operation it is stored when you 
+     // press the operation button.
     numBuffer: [],
-    result: "",
+    result: "", // Variable which holds the float of the result to be printed
 
     updateDisplay: function(button) {
-        if(this.deleteDisplayText == true) {
-            this.clearDisplay()
-            this.deleteDisplayText = false
+
+        /** Prints new charachters to the display 
+         * Called when any number button is pressed
+        */
+
+        if(this.deleteDisplayText == true) { // After setting a new operation(clicking +, -, *, /) it is needed to clear the numbers already 
+            this.clearDisplay()              // present in the display so the second number of the operation can be input. So there's a 
+            this.deleteDisplayText = false   // control variable which tells if the display needs to be clear or not.
         }
-        this.resetButton.textContent = "C"
-        this.display.textContent += button.textContent
+        this.resetButton.textContent = "C" // Sets Clear button to C since it starts with AC(All Clear) written on it
+        this.display.textContent += button.textContent // Adds the button text(number) to display text
     },
 
     clearDisplay: function() {
+
+        /** Clears display */
+
         this.display.textContent = ""
     },
 
     calcPercentage: function() {
+
+        /** Calculates percentage of present number in display 
+         * Function called when percentage button is pressed
+        */
+
         this.display.textContent = parseFloat((this.display.textContent)/100)  
     },
 
     flipSign: function() {
 
-        if(this.display.textContent[0] == "+") {
+        /** Flips the number sign(+ or -) 
+         * Function called when sign button is pressed
+        */
+
+        if(this.display.textContent[0] == "+") { // Sets the sign as - if the number is already positive
             this.display.textContent = this.display.textContent.replace("+", "-")
             return "-"
-        } else if(this.display.textContent[0] == "-") {
+        } else if(this.display.textContent[0] == "-") { // Sets the sign as + if the number is already negative
             this.display.textContent = this.display.textContent.replace("-", "+")
             return "+"
-        } else if((this.display.textContent[0] != "-") && (this.display.textContent[0] != "+")) {
-            this.display.textContent = `-${this.display.textContent}`
+        } else if((this.display.textContent[0] != "-") && (this.display.textContent[0] != "+")) { // if number doesn't has a sign it means it's
+            this.display.textContent = `-${this.display.textContent}`                             // postive by default so if sets the sign as -
             return "-"
         }
     },
 
     resetCalc: function() {
+
+        /** Resets all calcualtor functions 
+         * Function called when Clear button is pressed
+        */
+
         this.clearDisplay()
         this.resetButton.textContent = "AC"
         this.numBuffer = []
     },
 
     setOperation: function(button) {
-        this.deleteDisplayText = true
-        this.operation = button.textContent
-        this.numBuffer.push(parseFloat(this.display.textContent))
+
+        /** 
+         * Sets which operation should be executed when the "=" button is hit 
+         *  Function called when any operator button(+,-,*,/) is pressed
+        */
+
+        this.deleteDisplayText = true // Sets the delete display control variable as true(Explained in calc.updateDisplay)
+        this.operation = button.textContent // Sets the operation variable to the desired operation
+        this.numBuffer[0] = parseFloat(this.display.textContent) // Sets first number of the operation in the buffer(2 numbers only always)
     },
 
     exeOperation: function() {
-        this.numBuffer.push(parseFloat(this.display.textContent))
 
+        /** 
+         * Executes and display the operation results 
+         *  Function called by pressing equalButton 
+        */
+
+        this.numBuffer[1] = (parseFloat(this.display.textContent)) // Sets second number of the operation in the buffer
+
+        // Sets the result variable based on the 
         if(this.operation == "+") {
             this.result = this.numBuffer[0] + this.numBuffer[1]
         } else if(this.operation == "-") {
